b7e2f37ce25386dff91e8c883e21b0371ff565b0
tomasfelipeloaiza@gmail.com <shadow39456>
2019-08-05 17:26:07 -0500

    Se agrega en Login la busqueda de usuario en la BD

--- app/src/main/java/com/knoxcorporation/knxbank/Databases/conexionsqlitehelper.java
+++ app/src/main/java/com/knoxcorporation/knxbank/Databases/conexionsqlitehelper.java
@@ -1,6 +1,8 @@
 package com.knoxcorporation.knxbank.Databases;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -27,5 +29,17 @@ public class conexionsqlitehelper extends SQLiteOpenHelper {
         knxcBankdb.execSQL("DROP TABLE IF EXISTS cuenta");
         knxcBankdb.execSQL("DROP TABLE IF EXISTS transaccion");
         onCreate(knxcBankdb);
+    }
+
+    public Cursor Consultarusuario(String user, String pass)throws SQLException {
+        Cursor mcursor=null;
+
+            mcursor=this.getReadableDatabase().query(utilidades.TABLA_CLIENTE,new String[]{utilidades.CAMPO_ID,utilidades.CAMPO_DNI,utilidades.CAMPO_NOMBRE,utilidades.CAMPO_APELLIDOS,
+                    utilidades.CAMPO_EDAD,utilidades.CAMPO_CLAVE,utilidades.CAMPO_EMAIL},utilidades.CAMPO_DNI+"like'"+user+"'"+utilidades.CAMPO_CLAVE+"like'"+pass+"'",
+                    null,null,null,null);
+
+        return mcursor;
+
+
     }
 }
--- app/src/main/java/com/knoxcorporation/knxbank/LoginActivity.java
+++ app/src/main/java/com/knoxcorporation/knxbank/LoginActivity.java
@@ -2,13 +2,56 @@ package com.knoxcorporation.knxbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import com.knoxcorporation.knxbank.Databases.conexionsqlitehelper;
 
 public class LoginActivity extends AppCompatActivity {
+        EditText usuario,contrasena;
+        Button btnlogin,btnregister;
+        conexionsqlitehelper helper=new conexionsqlitehelper(this,"knxcBankdb",null,1);
+                ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Intent de registro desde login
+            btnregister=(Button)findViewById(R.id.btnRegister);
+            btnregister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i=new Intent(getApplicationContext(),RegisterActivity.class);
+                    startActivity(i);
+                }
+            });
+        //Button login
+            btnlogin=(Button)findViewById(R.id.btnLogin);
+            btnlogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    usuario=(EditText) findViewById(R.id.registerDNI);
+                    contrasena=(EditText) findViewById(R.id.registerPassword);
+                    try{
+                        Cursor cursor= helper.Consultarusuario(usuario.getText().toString(),contrasena.getText().toString());
+                        if(cursor.getCount()>0){
+                            Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(i);
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Usuario o contraseña invalido",Toast.LENGTH_LONG).show();
+                        }
+                        usuario.setText("");
+                        contrasena.setText("");
+                    }catch(SQLException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+
     }
 }
