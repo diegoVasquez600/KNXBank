b951721e16c7a82cf47a9079659d1c9107a60221
tloaiza1 <tomasfelipeloaiza@gmail.com>
2019-08-13 20:15:45 -0500

    Se agrega validacion de login para firebase SIN TESTEAR

--- app/src/main/java/com/knoxcorporation/knxbank/LoginActivity.java
+++ app/src/main/java/com/knoxcorporation/knxbank/LoginActivity.java
@@ -1,16 +1,20 @@
 package com.knoxcorporation.knxbank;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.*;
+import com.knoxcorporation.knxbank.Entidades.Cliente;
 public class LoginActivity extends AppCompatActivity {
         EditText usuario,contrasena;
         Button btnlogin,btnregister;
+        DatabaseReference databasereference;
 
 
     @Override
@@ -18,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //Intent de registro desde login
+        initializeFirebase();
             btnregister = findViewById(R.id.btnRegister);
             btnregister.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -33,10 +38,46 @@ public class LoginActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     usuario= findViewById(R.id.registerDNI);
                     contrasena = findViewById(R.id.registerPassword);
+                    final String user=usuario.getText().toString(),pass=contrasena.getText().toString();
+                    //Busqueda de usuario
+                    databasereference.orderByChild("telefono").equalTo(user).addValueEventListener(new ValueEventListener(){
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                           for (DataSnapshot child : dataSnapshot.getChildren()) {
+                                final String model;
+                                model = child.getValue(Cliente.class);
+                                //Validacion usuario sea igual a edit text
+                                if (!(model == user )) {
+                                    //Busqueda clave
+                                    databasereference.orderBychild("clave").equalto(pass).addValueEventListener(new ValueEventListener(){
+                                        for (DataSnapshot child: datasnapshot.getChildren()){
+                                            model=child.getValue(Cliente.class);
+                                            //Validacion clave sea igual a edittext pass
+                                            if (!(model==pass)){
+                                                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                                                startActivity(i);
+                                            }else{
+                                                Toast.makeText(getApplicationContext(),"Contraseña erronea",Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                                }else{
+                                    Toast.makeText(getApplicationContext(),"Usuario erroneo",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+
 
                 }
             });
 
 
+    }
+
+    private void initializeFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase=firebaseDatabase.getInstance();
+        databasereference=firebaseDatabase.getReference();
     }
 }
