0cf599b27d317b77be9c8b5ed05a60e378c4e33a
Diego Vasquez <diegovasquezcolombia@gmail.com>
2019-08-15 15:10:45 -0500

    Changed to Firebase Update

--- app/src/main/java/com/knoxcorporation/knxbank/LoginActivity.java
+++ app/src/main/java/com/knoxcorporation/knxbank/LoginActivity.java
@@ -8,6 +8,9 @@ import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
         EditText usuario,contrasena;
         Button btnlogin,btnregister;
@@ -18,25 +21,35 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //Intent de registro desde login
-            btnregister = findViewById(R.id.btnRegister);
-            btnregister.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent toRegister = new Intent(getApplicationContext(),RegisterActivity.class);
-                    startActivity(toRegister);
-                }
-            });
+        btnregister = findViewById(R.id.btnRegister);
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toRegister = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(toRegister);
+            }
+        });
         //Button login
-            btnlogin= findViewById(R.id.btnLogin);
-            btnlogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    usuario= findViewById(R.id.registerDNI);
-                    contrasena = findViewById(R.id.registerPassword);
+        btnlogin = findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuario = findViewById(R.id.registerDNI);
+                contrasena = findViewById(R.id.registerPassword);
 
-                }
-            });
+            }
+        });
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            Intent toWallet = new Intent(getApplicationContext(), WalletActivity.class);
+            //Kill all other Activities by D'V
+            toWallet.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(toWallet);
+        }
     }
 }
--- app/src/main/java/com/knoxcorporation/knxbank/RegisterActivity.java
+++ app/src/main/java/com/knoxcorporation/knxbank/RegisterActivity.java
@@ -1,22 +1,26 @@
 package com.knoxcorporation.knxbank;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.knoxcorporation.knxbank.Adapters.CountryData;
 import com.knoxcorporation.knxbank.Entidades.Cliente;
-import com.knoxcorporation.knxbank.Entidades.Cuenta;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.*;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -24,10 +28,10 @@ import java.util.concurrent.TimeUnit;
 public class RegisterActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    String codeSent;
     EditText names, lastNames,age,idNumber,password,phoneNumber,email;
+    Spinner countryCodes;
     Button registerButton,toLoginButton;
-
+    ProgressBar loading;
     DatabaseReference databasereference;
     FirebaseDatabase firebaseDatabase;
     @Override
@@ -42,9 +46,14 @@ public class RegisterActivity extends AppCompatActivity {
         idNumber = findViewById(R.id.registerDNI);
         password = findViewById(R.id.registerPassword);
         phoneNumber = findViewById(R.id.registerPhone);
+        countryCodes = findViewById(R.id.countryCodes);
         email = findViewById(R.id.registerEmail);
         registerButton = findViewById(R.id.buttonRegister);
+        loading = findViewById(R.id.progressBar);
         toLoginButton = findViewById(R.id.buttonToLogin);
+        loading.setVisibility(View.INVISIBLE);
+        registerButton.setVisibility(View.VISIBLE);
+        countryCodes.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
         initializeFirebase();
 
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -52,10 +61,11 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //DONE: Register User information and phone auth verification
                 //DONE: Register User information in the database
+                //Area Code Method()
+                registerButton.setVisibility(View.INVISIBLE);
+                loading.setVisibility(View.VISIBLE);
+                areaCode();
                 register();
-                //Intent to VerificationActivity
-
-
             }
         });
         toLoginButton.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +79,31 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    private void areaCode() {
+        String code = CountryData.countryAreaCodes[countryCodes.getSelectedItemPosition()];
+        String phone = phoneNumber.getText().toString().trim();
+        //Conditional if phone is empty
+        String phoneError = getResources().getString(R.string.phoneError);
+        String phonelength = getResources().getString(R.string.phonelenght);
+        if (phone.isEmpty()){
+            phoneNumber.setError(phoneError);
+            phoneNumber.requestFocus();
+            return;
+        }if (phone.length() < 10){
+            phoneNumber.setError(phonelength);
+            phoneNumber.requestFocus();
+            return;
+        }
+        //phoneNumber
+        String numberPhone = "+" + code + phone;
+        //Intent to VerificationActivity
+        Intent toVerify = new Intent(getApplicationContext(), VerificationActivity.class);
+        toVerify.putExtra("phoneNumber", numberPhone);
+        startActivity(toVerify);
+    }
+
+
+    //Method register
     private void register() {
         String Nombre=names.getText().toString(),Apellido=lastNames.getText().toString(),Edad=age.getText().toString(),DNI=idNumber.getText().toString(),
                 pass=password.getText().toString(),telefono=phoneNumber.getText().toString(),correo=email.getText().toString();
@@ -83,54 +118,16 @@ public class RegisterActivity extends AppCompatActivity {
         user.setCorreo(correo);
         databasereference.child("Cliente").child(user.getID()).setValue(user);
         Toast.makeText(getApplicationContext(),"Agregado correctamente",Toast.LENGTH_LONG).show();
-        verifyPhoneNumber();
     }
+
+    //Realtime Database Method()
     private void initializeFirebase() {
         FirebaseApp.initializeApp(this);
-        firebaseDatabase=firebaseDatabase.getInstance();
-        databasereference=firebaseDatabase.getReference();
+        firebaseDatabase = firebaseDatabase.getInstance();
+        databasereference = firebaseDatabase.getReference();
 
     }
 
-    private void verifyPhoneNumber() {
-        //TODO: verifyPhoneNumber Method
-        //To string from EditText
-        String phone = phoneNumber.getText().toString();
-        //Conditional if phone is empty
-        String phoneError = getResources().getString(R.string.phoneError);
-        String phonelength = getResources().getString(R.string.phonelenght);
-        if (phone.isEmpty()){
-            phoneNumber.setError(phoneError);
-            phoneNumber.requestFocus();
-            return;
-        }if (phone.length() < 10){
-            phoneNumber.setError(phonelength);
-            phoneNumber.requestFocus();
-            return;
-        }
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phone,        // Phone number to verify
-                60,                 // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                this,               // Activity (for callback binding)
-                mCallbacks);        // OnVerificationStateChangedCallbacks
-    }
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
 
-        }
-
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-
-        }
-
-        @Override
-        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-            super.onCodeSent(s, forceResendingToken);
-            codeSent = s;
-        }
-    };
 
 }
